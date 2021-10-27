60a58b8e52bfb8afa64b5071ce1789b20d0693aa
Isseinie <isseinie.calviac@ens-rennes.fr>
2021-10-27 09:54:57 +0200

    cmarrt added

---
+++ include/CMARRT.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <list>
+#include <memory>
+#include <set>
+#include <map>
+#include <stack>
+#include <vector>
+#include <utility>
+#include <unordered_set>
+#include <queue>
+#include <CTNOrderingStrategy.hpp>
+#include <ConflictSelectionStrategy.hpp>
+#include <ConstraintTreeNode.hpp>
+#include <Instance.hpp>
+#include <LowLevel.hpp>
+#include <Objective.hpp>
+#include <Solver.hpp>
+#include <boost/heap/fibonacci_heap.hpp>
+#include <FloydWarshall.hpp>
+
+namespace cmarrt {
+
+    
+}
