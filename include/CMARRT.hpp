c9216054bc9bd850c166344599147a87d59052c9
Isseinie <isseinie.calviac@ens-rennes.fr>
2021-11-10 09:51:07 +0100

    exploration tree

--- include/CMARRT.hpp
+++ include/CMARRT.hpp
@@ -24,10 +24,21 @@ namespace cmarrt {
 template <class GraphMove, class GraphComm>
 class CMARRT : public Solver<GraphMove, GraphComm> {
  private:
-  class Exploration {
+ float p = 0.2; //biasis
+ int step = 5; //to define
+  class ExplorationTree {
    public:
-    // explorationtree_; structure ?
+    std::vector<std::shared_ptr<Configuration>> vertices_;
+	  std::vector<std::shared_ptr<Configuration>> parents_; //for edges
+
     const Instance<GraphMove, GraphComm>& instance_;
+
+    explicit ExplorationTree(const Instance<GraphMove, GraphComm>& instance) 
+    : instance_(instance), vertices_(), parents_() {
+      std::shared_ptr<Configuration> start = std::make_shared<Configuration>();
+      for (size_t agt = 0; agt < instance.start().size(); agt++) start->PushBack(instance.start()[agt]);
+      vertices_.push_back(start);
+    };
 
     bool has(const std::shared_ptr<Configuration>& config) {
       for (auto found : this->explorationtree_) {
@@ -43,18 +54,23 @@ class CMARRT : public Solver<GraphMove, GraphComm> {
         }
       }
     }
+
+    void extend(tree){
+      std::shared_ptr<Configuration> c_rand = pick_rand(p, instance_->goal());
+      
+    }
   }
 
-  std::vector<std::shared_ptr<Configuration>> vertices_;
-	std::vector<std::shared_ptr<Configuration>> parents_; //for edges
+  ExplorationTree<GraphMove, GraphComm> explorationtree_;
+
+  std::vector<std::shared_ptr<Configuration>> ComputePath(ExplorationTree tree, Instance instance)
+  {
+    std::vector<std::shared_ptr<Configuration>> exec;
+  }
 
  public:
   CMARRT(const Instance<GraphMove, GraphComm>& instance, const Objective& objective)
-      : Solver<GraphMove, GraphComm>(instance, objective), exec_() {
-        	std::shared_ptr<Configuration> start = std::make_shared<Configuration>();
-          for (size_t agt = 0; agt < instance.start().size(); agt++) start->PushBack(instance;start()[agt]);
-					vertices_.push_back(start);
-			}
+      : Solver<GraphMove, GraphComm>(instance, objective), explorationtree_(instance){};
 
   bool StepCompute() override {
     if (explorationtree_.has(instance_->goal())) {
